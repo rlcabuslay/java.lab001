b69d0c3f9542158634d99de6a40b013830a41f97
Ryan Cabuslay <rlcabuslay1@up.edu.ph>
2022-11-07 21:15:34 +0800

    remove: comments in Answer1.java

--- src/Answer1.java
+++ src/Answer1.java
@@ -7,12 +7,6 @@ public class Answer1 {
 		Scanner in = new Scanner(System.in);
 		int mark = in.nextInt();
 		
-//		int mark = 0;
-//		int mark = 49;
-//		int mark = 50;
-//		int mark = 51;
-//		int mark = 100;
-		
 		System.out.println("The mark is " + mark);
 		 
 	    if (mark >= 50) {
